f5a4358a2c86332b68e93d9af0234dd37c9055ae
ianMuchesia <ianmuchesia@yahoo.com>
2023-06-02 08:57:43 +0300

    Variables

---
+++ 02_variable.php
@@ -0,0 +1,26 @@
+<?php
+/*$name = 'Brad';
+$age = 35;
+$hash_kids = [ "ian", "Shirleen"];*/
+
+//echo $name, $age;
+
+//var_dump($hash_kids);
+
+
+//concatenation
+/*echo $name . ' is' .  $age . ' Years old';
+echo "$name is $age Years old";*/
+
+$x = '5' + '5';
+var_dump($x);
+$x = 5 + 5;
+var_dump($x);
+
+$x = '5'. '5';
+var_dump($x);
+
+
+//constants
+define('HOST', 'localhost');
+define('DB_NAME', 'dev_db'); 
