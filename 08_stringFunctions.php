a824a652a1c5c116cd1ea126bf6eb0fac8a1f6a7
ianMuchesia <ianmuchesia@yahoo.com>
2023-06-02 12:11:34 +0300

    day one

---
+++ 08_stringFunctions.php
@@ -0,0 +1,30 @@
+<?php
+
+
+$string = 'Hello World';
+
+//strlen length o f string
+
+//strpos , position of a letter in a sring
+
+//strrev reverse a string
+
+//strtolower converst it to lower case
+
+//strtoupper converts all tio uppercase
+
+//str_replace
+
+
+//str_starts_with
+
+//str_ends_with
+
+
+$string2 = '<h1>Hello</h1>';
+echo htmlspecialchars($string2);
+
+
+printf('%s likes to %s', 'Msodoki', 'Code');
+
+
