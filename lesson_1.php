c802023defee82d0250cfd21d30ca5e628726ced
ianMuchesia <ianmuchesia@yahoo.com>
2023-06-02 08:41:56 +0300

    first commit

---
+++ lesson_1.php
@@ -0,0 +1,31 @@
+<?php
+echo 123 ,"hello wold";
+
+
+print 123;
+
+
+//print_r is used in printing arrays 
+//print_r(["1", 2 , 3]);
+
+//var_dump Return more infromation like data type and length
+var_dump(123);
+
+//outputs a tring similar tp var_dump, output represenattion of the variable
+var_export(123);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo"Hello world"; ?></h1>
+</body>
+</html>
+
+
